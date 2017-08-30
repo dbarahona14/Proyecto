3301d640c2dd0c3cf867587adec09a05f688c2cd
dbarahona14 <31461134+dbarahona14@users.noreply.github.com>
2017-08-30 00:04:07 -0300

    Add files via upload

---
+++ Freya-Style/src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    private @FXML TextField inputText;
+    public Controller() {
+
+    }
+}
